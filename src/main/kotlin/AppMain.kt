1f05429c2c7c5131e4a27d93ec80d9e0ea9a9808
Moomink <52168914+Moomink@users.noreply.github.com>
2020-03-08 17:09:55 +0900

    壁の追加

--- src/main/kotlin/AppMain.kt
+++ src/main/kotlin/AppMain.kt
@@ -2,22 +2,39 @@ import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.scene.layout.Pane
+import javafx.scene.paint.Color
 import javafx.stage.Stage
 
 class AppMain : Application() {
+    val HEIGHT:Double = 600.0
+    val WIDTH:Double = 800.0
 
     override fun start(primaryStage: Stage) {
-        primaryStage.title = "KotlinでJavaFX"
-        val fxml = javaClass.getResource("fxml/hello.fxml")
+        primaryStage.title = "HogeHoge"
+        val fxml = javaClass.getResource("fxml/board.fxml")
 
         val root = FXMLLoader.load<Pane>(fxml)
 
         val scene = Scene(root)
 
+        scene.fill = Color.TRANSPARENT
+        root.style = "-fx-background-color: transparent"
+
+
         primaryStage.scene = scene
 
+        primaryStage.height = 700.0
+
+        primaryStage.width = 800.0
+
+        val animation = Animation(800.0,700.0,25.0)
+
+        root.children.add(animation.Test())
         primaryStage.show()
 
+//        Timer().schedule(1000,1000){
+//            println("縦:${primaryStage.height} 横:${primaryStage.width}")
+//        }
     }
 
 }
